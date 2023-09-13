64f918ff6189225f83cda4c6ef8cb315e3925bee
Chives Network <reactchives@gmail.com>
2023-09-12 19:37:08 -0700

    get_trusted_peers

--- apps/chivesweave/src/ar_peers.erl
+++ apps/chivesweave/src/ar_peers.erl
@@ -166,7 +166,13 @@ get_peer_performances(Peers) ->
 -if(?NETWORK_NAME == "chivesweave.mainnet").
 get_trusted_peers() ->
 	{ok, Config} = application:get_env(chivesweave, config),
-	Config#config.peers.
+	case Config#config.peers of
+		[] ->
+			ArweavePeers = ["node1.chivesweave.net", "node2.chivesweave.net"],
+			resolve_peers(ArweavePeers);
+		Peers ->
+			Peers
+	end.
 -else.
 get_trusted_peers() ->
 	{ok, Config} = application:get_env(chivesweave, config),
